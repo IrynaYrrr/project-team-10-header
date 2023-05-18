37a4f61cd37f3edb7fcab8a2f4efe7c0e3c92dc3
Iryna <78929283+IrynaYrrr@users.noreply.github.com>
2023-05-18 04:49:52 +0300

    update code

--- src/js/header/header.js
+++ src/js/header/header.js
@@ -1,39 +1,3 @@
-// const theBody = document.querySelector('body');
-// const openNav = document.querySelector('.header-menu button');
-// const closeNav = document.querySelector('.header-close-nav button');
-// const HeaderNavbar = document.querySelector('.header-navbar');
-// const toggleButton = document.querySelector('.header-toggle');
-
-// if(window.location.pathname === "/index.html") {
-//   document.querySelector('.header-shopping').classList.remove('header-active');
-//   document.querySelector('.header-home').classList.add('header-active');
-// } else if(window.location.pathname === "/shopping-list.html") {
-//   document.querySelector('.header-home').classList.remove('header-active');
-//   document.querySelector('.header-shopping').classList.add('header-active');
-// }
-
-// function show() {
-//   HeaderNavbar.classList.toggle('show');
-//   toggleButton.style.display = 'none';
-// }
-
-// function hide() {
-//   HeaderNavbar.classList.toggle('show');
-//   toggleButton.style.display = 'block';
-
-// }
-
-// openNav.onclick = show;
-// closeNav.onclick = hide;
-
-// $('.switch-btn').click(function(){
-//   $(this).toggleClass('switch-on');
-// });
-
-
-
-
-
 const switchBtn = document.querySelector('.header__switch-btn');
 const switchOn = document.querySelector('.header__switch-on');
 const headerContainer = document.querySelector('.header__container');
